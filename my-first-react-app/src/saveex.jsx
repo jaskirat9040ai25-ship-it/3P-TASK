3f96926190006621062d69d1d1dcecb55a1d3893
jaskirat-stack <jaskirtdhillon9033@gmail.com>
2026-09-18 17:07:07 +0530

    Remove unused file

--- my-first-react-app/src/saveex.jsx
+++
@@ -1,9 +0,0 @@
-function SaveButton() {
-    function handleSave() {
-        console.log("pretend we just saved the form");
-    }
-    return(
-        <button  onClick={handleSave} className="rounded bg-teal-600 px-4 py-2 text-white">Save</button>
-    );
-}
-export default SaveButton;
