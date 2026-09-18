f93c811dda07788558adc98576c47ac5506de0f1
jaskirat-stack <jaskirtdhillon9033@gmail.com>
2026-09-18 17:08:12 +0530

    Remove unused file

--- my-first-react-app/src/order.jsx
+++
@@ -1,9 +0,0 @@
-function OrderStatus({status}){
-    let message;
-    if(status=== "pending") message = <p>we have recieved your order</p>;
-    else if(status === "shipped")message = <p>on its way</p>;
-    else if (status === "delivered")message = <p>delivered . enjoy</p>;
-    else message = <p> unknown status</p>;
-    return <div className="status">{message}</div>;
-}
-export default OrderStatus;
